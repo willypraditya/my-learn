e62b07b1c8e37774df57a30990b9455e203aad09
Willy <w.praditya@yahoo.com>
2022-03-06 02:39:06 +0700

    init: mylearn project structure

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-use-before-define': 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    semi: [2, 'always'],
+    'no-unused-vars': 'warn',
+  },
+};
