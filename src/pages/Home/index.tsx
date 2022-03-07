d4c00706a00e7ad78e72f7bfb09e37ee12459563
Willy <w.praditya@yahoo.com>
2022-03-08 04:03:21 +0700

    enhancement: added loading in Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from 'react';
 
-import { Card, Typography } from 'antd';
+import { Card, Spin, Typography } from 'antd';
 
 import ClassModal from '@components/ClassModal';
 
@@ -10,23 +10,31 @@ import useHomeHooks from './hooks';
 import styles from './index.module.scss';
 
 const Home = (): ReactElement => {
-  const { data, modalClassId, modalVisible, setModalVisible, onClassClick } = useHomeHooks();
+  const { loading, data, modalClassId, modalVisible, setModalVisible, onClassClick } = useHomeHooks();
 
   return (
     <div data-testid="home" className={styles.home}>
       <Typography className={styles.home__title}>MyLearn</Typography>
       <div className={styles.home__content}>
-        {data &&
-          data.items.map((item: Class) => (
-            <Card
-              key={item.id}
-              size="small"
-              className={styles.home__content__card}
-              onClick={() => onClassClick(item.id)}
-            >
-              <Typography className={styles.home__content__card__title}>{item.name}</Typography>
-            </Card>
-          ))}
+        {loading ? (
+          <div style={{ textAlign: 'center' }}>
+            <Spin style={{ textAlign: 'center' }} />
+          </div>
+        ) : (
+          <div>
+            {data &&
+              data.items.map((item: Class) => (
+                <Card
+                  key={item.id}
+                  size="small"
+                  className={styles.home__content__card}
+                  onClick={() => onClassClick(item.id)}
+                >
+                  <Typography className={styles.home__content__card__title}>{item.name}</Typography>
+                </Card>
+              ))}
+          </div>
+        )}
       </div>
       <ClassModal classId={modalClassId} modalVisible={modalVisible} setModalVisible={setModalVisible} />
     </div>
